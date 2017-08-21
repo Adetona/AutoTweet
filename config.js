32cdaa58876604f5971319d564f8b69c7fbfd95d
Adetona <adetonaabiodun12@gmail.com>
2017-08-21 01:26:02 +0100

    Added files -- procfile and server.js

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+  consumer_key:         '65EmpGTOKXXkjHR9tI5Jiio01',
+  consumer_secret:      'FZ0lYU5dUJA4YTuqPtZ4q5NfXLbXFuz4E9xgA49ij6Tc8YAcol',
+  access_token:         '2963547257-9N7VF9O9gLjQQTtJhRVjk8chJQivg0zTMV2dy9E',
+  access_token_secret:  'QMnRuSd0zbh3HbZ09wcVUohOEAol5W6HrABPyNF0gf0fY'
+
+
+}
+
+module.exports = config;
---
+++ server.js
@@ -0,0 +1,66 @@
+var Twit = require('twit')
+
+var fs = require('fs'),
+    path = require('path'),
+    Twit = require('twit'),
+
+    config = require(path.join(__dirname, 'config.js'));
+
+var T = new Twit(config);
+
+
+
+var bot = ['blad..']; 
+
+var bot = bot[Math.floor(Math.random() * bot.length)];
+var post = function () {
+
+T.post('statuses/update', { status: bot }, function(err, data, response) {
+
+console.log('success'); 	    
+
+});
+
+}
+
+//post(); 
+
+
+var ontime = require('ontime')
+
+ontime({
+    cycle: '21:50:00'
+}, function (ot) {
+    // do your job here
+
+post();
+console.log('Hi'); 
+ot.done()
+    return
+})
+
+
+//setInterval(post, 10000);
+
+/*setInterval(function() {
+T.post('statuses/update', { status: bot }, function(err, data, response) {
+
+console.log('success'); 	    
+
+});
+
+ }*/ 
+
+
+ 
+
+/*setInterval(
+T.post('statuses/update', { status: '....' }, function(err, data, response) {
+
+	console.log(data); 
+});
+
+}
+  //post_tweet (),  
+  10000
+);*/ 
